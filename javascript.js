ae0df7d51d170000b8d0c196eabe0eb3bebcfda3
Kieran <kieran.webdevelopment@gmail.com>
2022-12-08 16:44:37 +0000

    css almost complete

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ questionContainers.forEach((item) =>
   item.addEventListener('click', function () {
     this.children[1].classList.toggle('hidden');
     this.children[0].children[0].classList.toggle('rotate-icon');
+    this.children[0].classList.toggle('h4-bold');
+    console.log(this.children[0]);
   })
 );
 
